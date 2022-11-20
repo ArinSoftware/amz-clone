537d82b3242e64b17acf696863637ab77b6dd527
gurcancekic <arinyazilim@gmail.com>
2022-11-20 13:15:02 +0300

    DB connection and env settings

--- server/server.js
+++ server/server.js
@@ -1,7 +1,20 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const morgan = require('morgan');
+const dotenv = require('dotenv');
+
+dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT;
+
+mongoose.connect(process.env.DB_URI, (err) => {
+  if (err) {
+    console.log('err');
+  } else {
+    console.log('Connected to the DB succesfully');
+  }
+});
 
 //middlewares
 app.use(morgan('dev'));
@@ -16,9 +29,9 @@ app.post('/', (req, res) => {
   console.log('BODY', req.body);
 });
 
-app.listen(port, (err) => {
+app.listen(PORT, (err) => {
   if (err) {
-    console.log('Err at server running:', err);
+    console.log(`Err at server running at port: ${PORT}`, err);
   } else {
     console.log('Application running on port: 3000');
   }
