cd47af17fd35fad8550de5f62f65d9f5365a1564
gurcancekic <arinyazilim@gmail.com>
2022-12-01 23:00:01 +0300

    end of backedn API

--- server/routes/product.js
+++ server/routes/product.js
@@ -72,7 +72,64 @@ router.get('/products/:id', async (req, res) => {
 });
 
 // Update a product PUT
+router.put('/products/:id', async (req, res) => {
+  const result = await cloudinary.uploader.upload(
+    req.files.photo.tempFilePath,
+    {
+      use_filename: true,
+      folder: 'amz_clone',
+    }
+  );
+
+  try {
+    let product = await Product.findOneAndUpdate(
+      { _id: req.params.id },
+      {
+        $set: {
+          title: req.body.title,
+          price: req.body.price,
+          category: req.body.categoryID,
+          photo: result.secure_url,
+          description: req.body.description,
+          owner: req.body.ownerID,
+          stockQuantity: req.body.stockQuantity,
+        },
+      },
+      { upsert: true }
+    );
+
+    // remove image from tmp file
+    fs.unlinkSync(req.files.photo.tempFilePath);
+
+    res.json({
+      success: true,
+      updatedProduct: product,
+    });
+  } catch (error) {
+    res.status(500).json({
+      status: false,
+      message: error.message,
+    });
+  }
+});
 
 // Delete a product DELETE
+router.delete('/products/:id', async (req, res) => {
+  try {
+    let product = await Product.findOneAndDelete({ _id: req.params.id });
+
+    if (product) {
+      res.json({
+        success: true,
+        message: 'The Product Successfully Deleted',
+      });
+    }
+  } catch (error) {
+    res.status(500).json({
+      status: false,
+      message: error.message,
+    });
+  }
+});
 
 module.exports = router;
